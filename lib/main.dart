b681921976f03f5b56a690684b36ed6dfe155000
ferre0160 <heydeferre@gmail.com>
2021-10-20 11:44:57 +0200

    home

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,8 @@ import 'package:gymigo/pages/login.dart';
 import 'package:gymigo/pages/home.dart';
 import 'package:gymigo/pages/day.dart';
 
-void main() => runApp(MaterialApp(
-
-  routes: {
-    '/login': (context) => Login(),
-    '/home': (context) => Home(),
-    '/day': (context) => Day(),
-  }
-));
+void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+      '/login': (context) => Login(),
+      '/home': (context) => Home(),
+      '/day': (context) => Day(),
+    }));
