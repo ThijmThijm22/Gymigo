505c3fad032521bbd80b69f1dd65e6775a9cc060
Thijmen Verstraete <verstraetethijmen@gmail.com>
2021-10-20 18:25:15 +0200

    rounded buttons

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'globals.dart';
+import '../widgets/roundButton.dart';
 
 class Login extends StatefulWidget {
   const Login({ Key? key }) : super(key: key);
@@ -16,13 +17,29 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Globals.background,
+      body: Center(
+        child: Column(
+          children:  [
+            SizedBox(height: 120),
 
-      appBar: AppBar(
-        title: Text('Gymigo'),
-        centerTitle: true,
-        backgroundColor: Globals.background,
+            Text(
+              'Gymigo',
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+                letterSpacing: 2,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
 
-      ),
+            SizedBox(height: 300),
+
+            RoundButton(bgColor: Globals.purple),
+            SizedBox(height: 40),
+            RoundButton(bgColor: Globals.background),
+          ],
+        ),
+      )
       
     );
   }
