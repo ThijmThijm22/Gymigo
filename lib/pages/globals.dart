c7900e2e1bf210215d139222c31fadd6c0fb0e0c
ferre0160 <heydeferre@gmail.com>
2021-10-22 22:14:00 +0200

    homepage commit 2

--- lib/pages/globals.dart
+++ lib/pages/globals.dart
@@ -5,4 +5,5 @@ class Globals {
   static Color purple = const Color.fromARGB(255, 153, 157, 255);
   static Color textGrey = const Color.fromARGB(255, 112, 110, 110);
   static Color textWhite = const Color.fromARGB(255, 255, 255, 255);
+  static Color boxGrey = const Color.fromARGB(255, 56, 56, 56);
 }
--- lib/widgets/dayCard.dart
+++ lib/widgets/dayCard.dart
@@ -1,22 +1,57 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:gymigo/pages/globals.dart';
 
 class DayCard extends StatefulWidget {
-  late String dayOfWeek;
-  late String title;
+  final String dayOfWeek;
+  final String typeWorkout;
 
-  DayCard({this.dayOfWeek = "", this.title = ""});
+  const DayCard({Key? key, this.dayOfWeek = "", this.typeWorkout = ""})
+      : super(key: key);
 
   @override
   _DayCardState createState() => _DayCardState();
 }
 
 class _DayCardState extends State<DayCard> {
+  get dayOfWeek => null;
+
+  get title => null;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20),
+      width: 360,
+      height: 120,
+      decoration: BoxDecoration(
+          color: Globals.boxGrey, borderRadius: BorderRadius.circular(8)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 6),
+            child: Text(
+              dayOfWeek ?? "No day selected",
+              style: TextStyle(
+                  color: Globals.purple,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+            child: Text(
+              title ?? "No title",
+              style: TextStyle(
+                  color: Globals.textWhite,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
