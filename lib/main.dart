ad9f686376f3cf7267dfa46888befe6ff2af0c7d
ferre0160 <heydeferre@gmail.com>
2021-10-23 22:10:06 +0200

    error troubleshooting + homepage scrollable maken

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,20 @@ import 'package:google_fonts/google_fonts.dart';
 // Firebase
 import 'package:firebase_core/firebase_core.dart';
 
-Future <void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding();
   await Firebase.initializeApp();
 
   runApp(MaterialApp(
-
-    debugShowCheckedModeBanner: false,
-
-    theme: ThemeData(
-      textTheme: GoogleFonts.manropeTextTheme(),
-      primaryColor: Colors.white,
-    ),
-
-    initialRoute: '/login',
-    routes: {
-      '/login': (context) => Login(),
-      '/home': (context) => Home(),
-      '/day': (context) => Day(),
-    }
-  ));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.manropeTextTheme(),
+        primaryColor: Colors.white,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/login': (context) => Login(),
+        '/home': (context) => Home(),
+        '/day': (context) => Day(),
+      }));
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gymigo/pages/day.dart';
 import 'package:gymigo/pages/globals.dart';
 import 'package:gymigo/widgets/dayCard.dart';
 
@@ -13,59 +14,67 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
-    
-  User? user = FirebaseAuth.instance.currentUser;
+    User? user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
       backgroundColor: Globals.background,
-      body: Wrap(children: <Widget>[
-        SafeArea(
-          child: Container(
-              margin: EdgeInsets.only(top: 8),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  'Gymigo',
-                  style: TextStyle(color: Globals.textGrey, fontSize: 15),
+      body: SingleChildScrollView(
+        child: Wrap(children: <Widget>[
+          SafeArea(
+            child: Container(
+                margin: EdgeInsets.only(top: 8),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    'Gymigo',
+                    style: TextStyle(color: Globals.textGrey, fontSize: 15),
+                  ),
+                )),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 15, top: 20),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                user == null
+                    ? "Welcome Back"
+                    : "Welcome Back, " + user.email.toString(),
+                style: TextStyle(
+                  color: Globals.purple,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-              )),
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 15, top: 20),
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              user == null ? "Welcome Back" : "Welcome Back, " + user.email.toString(),
-              style: TextStyle(
-                color: Globals.purple,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 15, top: 8),
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              'This is your week:',
-              style: TextStyle(
-                color: Globals.textWhite,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
+          Container(
+            margin: EdgeInsets.only(left: 15, top: 8),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'This is your week:',
+                style: TextStyle(
+                  color: Globals.textWhite,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-        DayCard(),
-        DayCard(),
-        DayCard(),
-        DayCard()
-      ]),
+          DayCard(),
+          DayCard(),
+          DayCard(),
+          DayCard(),
+          DayCard(),
+          DayCard(),
+          DayCard()
+          // checken voor enumeraties in Flutter: enum maken met dagen van
+          // de week en dan elke card een vaste waarden geven aan de hand
+          // van de card & dag. card 1 = altijd Monday, ...
+        ]),
+      ),
     );
   }
 }
