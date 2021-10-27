38b1bfc780b99f80b649763c462e3cee9e88884e
ferre0160 <heydeferre@gmail.com>
2021-10-27 11:57:27 +0200

    Homepage Final + DayCard & Day page 1

---
+++ lib/widgets/workoutCard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:gymigo/pages/globals.dart';
+
+class Workout extends StatefulWidget {
+  const Workout({Key? key}) : super(key: key);
+
+  @override
+  _WorkoutState createState() => _WorkoutState();
+}
+
+class _WorkoutState extends State<Workout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
