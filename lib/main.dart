a97854a3f16d78273d062e9f43a6a51974c34bfd
ferre0160 <heydeferre@gmail.com>
2021-11-08 19:55:08 +0100

    Eerste changes op daypage met de TextField widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ Future<void> main() async {
         textTheme: GoogleFonts.manropeTextTheme(),
         primaryColor: Colors.white,
       ),
-      initialRoute: '/login',
+      initialRoute: '/home',
       routes: {
         '/welcome': (context) => Welcome(),
         '/login': (context) => Login(),
--- lib/pages/day.dart
+++ lib/pages/day.dart
@@ -16,18 +16,20 @@ class _DayState extends State<Day> {
       backgroundColor: Globals.background,
       body: Wrap(children: <Widget>[
         Container(
-          margin: EdgeInsets.only(left: 15),
+          margin: EdgeInsets.only(left: 23),
           child: Align(
             alignment: Alignment.topLeft,
             child: TextField(
+              maxLength: 35,
               style: TextStyle(
                   color: Globals.purple,
                   fontSize: 24,
                   fontWeight: FontWeight.bold),
               decoration: InputDecoration(
+                counterText: "",
                 border: InputBorder.none,
-                hintText: "What will you train?",
-                hintStyle: TextStyle(fontSize: 20, color: Globals.textWhite),
+                hintText: "What's in today's workout?",
+                hintStyle: TextStyle(fontSize: 24, color: Globals.textGrey),
               ),
             ),
           ),
@@ -42,13 +44,17 @@ class _DayState extends State<Day> {
             child: Align(
               alignment: Alignment.topLeft,
               child: TextField(
+                keyboardType: TextInputType.multiline,
+                minLines: 1,
+                maxLines: 5,
                 style: TextStyle(
                     color: Globals.textGrey,
                     fontSize: 15,
                     fontWeight: FontWeight.bold),
                 decoration: InputDecoration(
                   border: InputBorder.none,
-                  hintText: "Notes about this workout:",
+                  hintText:
+                      "Write here your notes for today's workout session ...",
                   hintStyle: TextStyle(fontSize: 15, color: Globals.textWhite),
                 ),
               ),
@@ -58,6 +64,7 @@ class _DayState extends State<Day> {
       ]),
       appBar: AppBar(
         title: Text(
+          // UPGRADE: de datum zou automatisch moeten veranderen naar de dag zelf
           'Maandag, 25 oktober 2021',
           style: TextStyle(color: Globals.textGrey, fontSize: 15),
         ),
